eb4567575159021bc18c242c74a5e7d04f4883b2
Michael McCrea <mccrea.michael@gmail.com>
2020-03-07 12:22:44 +0200

    schroeder allpass: correct off-by-one error in init tap position

--- matlab/functions/schrdAllpass.m
+++ matlab/functions/schrdAllpass.m
@@ -8,14 +8,12 @@ function Y = schrdAllpass(X, g, M)
     %   Y: output signal, equal in size to X (TBD)
     
     N = length(X);
-    tap = N-M;      % delay pointer
+    tap = N-(M-1);     % delay pointer
 
-    Y = zeros(N,1); % output buffer
+    Y = zeros(N,1);    % output buffer
     for n=1:N
         Y(n) = (-g*X(n)) + (g*Y(tap)) + X(tap); % DAFX, 2nd ed. eq. 5.19
         tap = tap+1;
-        if tap>N; tap = 1; end % wrap delay pointer
+        if tap>N; tap = 1; end  % wrap delay pointer
     end
 end
-
-% See: DAFX, 2nd ed. eq. 5.19
