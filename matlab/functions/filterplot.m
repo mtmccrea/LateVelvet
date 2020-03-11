7c78914e4189d634e9333e3b994979c3b649b7cc
Michael McCrea <mccrea.michael@gmail.com>
2020-03-11 14:37:50 +0200

    fixed the convolution path

--- matlab/functions/filterplot.m
+++ matlab/functions/filterplot.m
@@ -1,4 +1,4 @@
-function filterplot(Bcolor,Acolor,Bdiff,Adiff,fs)
+function filterplot(Bcolor,Acolor,Bdiff,Adiff,fs, gain_offsets)
 
 % filterplot(Bcolor,Acolor,Bdiff,Adiff,fs)
 % INPUT
@@ -8,51 +8,51 @@ function filterplot(Bcolor,Acolor,Bdiff,Adiff,fs)
 %Adiff : vector with denominator coefficients of differential filter
 % fs   : sample frequency [Hz]
 
-    [H,W] = freqz(Bcolor{1},Acolor{1});
-    H = H ./ H(1);
-    figure(1);
-    subplot(3,1,1)
-    semilogx(W*fs/(2*pi), mag2db(abs(H)));
-    ylabel('Magnitude');%xlabel('Frequency');
-    set(gca,'XTick',[125 250 500 1000 2000 4000 8000 16000]')    % Frequency points
-    set(gca,'YTick',[-100 -50 0  ]') % dB scale
-    axis([45 22500 -100 10])  %Scaling of axes
-    hold off
+[H,W] = freqz(Bcolor{1},Acolor{1});
+H = H ./ H(1);
+figure(1);
+subplot(3,1,1)
+semilogx(W*fs/(2*pi), mag2db(abs(H)) + gain_offsets(1));
+ylabel('Magnitude');%xlabel('Frequency');
+set(gca,'XTick',[125 250 500 1000 2000 4000 8000 16000]')    % Frequency points
+set(gca,'YTick',[-100 -50 0  ]') % dB scale
+axis([45 22500 -100 10])  %Scaling of axes
+hold off
+
+%plot the notch filters for each segment
+subplot(3,1,2)
+
+for n = 1 : size(Adiff,2)
+    [H1,W1] = freqz(Bdiff(:,n),Adiff(:,n));
+    semilogx(W1*fs/(2*pi), mag2db(abs(H1)));
+    H1 = H1 ./ H1(1);
+    hold on
     
-      %plot the notch filters for each segment 
-    subplot(3,1,2)
+end
+ylabel('Magnitude');%xlabel('Frequency');
+set(gca,'XTick',[125 250 500 1000 2000 4000 8000 16000 ]')    % Frequency points
+set(gca,'YTick',[-10 -5 0  ]') % dB scale
+axis([45 22500 -8 1])  %Scaling of axes
 
-    for n = 1 : size(Adiff,2)
-        [H1,W1] = freqz(Bdiff(:,n),Adiff(:,n));
-        semilogx(W1*fs/(2*pi), mag2db(abs(H1)));
-        H1 = H1 ./ H1(1);
-        hold on 
+% plot the estimated frequency response for 1. segment
+subplot(3,1,3)
+semilogx(W*fs/(2*pi), mag2db(abs(H)));
 
-    end
- ylabel('Magnitude');%xlabel('Frequency');
-    set(gca,'XTick',[125 250 500 1000 2000 4000 8000 16000 ]')    % Frequency points
-    set(gca,'YTick',[-10 -5 0  ]') % dB scale
-    axis([45 22500 -8 1])  %Scaling of axes
+hold on
 
- % plot the estimated frequency response for 1. segment 
-    subplot(3,1,3)
-    semilogx(W*fs/(2*pi), mag2db(abs(H)));
+for n = 1 : size(Acolor,2)
+    [H2,W2] = freqz(Bcolor{n},Acolor{n});
+    H2 = H2 ./ H2(1);
+    semilogx(W2*fs/(2*pi), mag2db(abs(H2)) + gain_offsets(n));
+    offset= 10 + n * 2;
+    hold on
+    
+end
 
-    hold on              
-       
-               for n = 1 : size(Acolor,2)
-        [H2,W2] = freqz(Bcolor{n},Acolor{n});
-        H2 = H2 ./ H2(1);
-        semilogx(W2*fs/(2*pi), mag2db(abs(H2)));
-         offset= 10 + n * 2;
-        hold on 
 
-        end
-            
-
-    ylabel('Magnitude');xlabel('Frequency');
-    set(gca,'XTick',[125 250 500 1000 2000 4000 8000 16000]')    % Frequency points
-    set(gca,'YTick',[-100 -50 0  ]') % dB scale
-    axis([45 22500 -100 10])  %Scaling of axes
-    hold off
+ylabel('Magnitude');xlabel('Frequency');
+set(gca,'XTick',[125 250 500 1000 2000 4000 8000 16000]')    % Frequency points
+set(gca,'YTick',[-100 -50 0  ]') % dB scale
+axis([45 22500 -100 10])  %Scaling of axes
+hold off
 end
