48221a3ee4fa1c12392b65dc7aa29f07ecfd15cd
Michael McCrea <mccrea.michael@gmail.com>
2020-03-12 17:28:15 +0200

    add plotting.

--- matlab/functions/filterplot.m
+++ matlab/functions/filterplot.m
@@ -8,18 +8,22 @@ function filterplot(Bcolor,Acolor,Bdiff,Adiff,fs, gain_offsets)
 %Adiff : vector with denominator coefficients of differential filter
 % fs   : sample frequency [Hz]
 
+rgb = genColors(green, 0.1, length(gain_offsets);
+
+% 10th-order LPC
 [H,W] = freqz(Bcolor{1},Acolor{1});
 H = H ./ H(1);
 figure(1);
 subplot(3,1,1)
 semilogx(W*fs/(2*pi), mag2db(abs(H)) + gain_offsets(1));
-ylabel('Magnitude');%xlabel('Frequency');
+ylabel('Magnitude'); %xlabel('Frequency');
 set(gca,'XTick',[125 250 500 1000 2000 4000 8000 16000]')    % Frequency points
 set(gca,'YTick',[-100 -50 0  ]') % dB scale
 axis([45 22500 -100 10])  %Scaling of axes
+colormap(rgb(1,:));
 hold off
 
-%plot the notch filters for each segment
+% plot the notch filters for each segment
 subplot(3,1,2)
 
 for n = 1 : size(Adiff,2)
@@ -27,17 +31,17 @@ for n = 1 : size(Adiff,2)
     semilogx(W1*fs/(2*pi), mag2db(abs(H1)));
     H1 = H1 ./ H1(1);
     hold on
-    
 end
 ylabel('Magnitude');%xlabel('Frequency');
 set(gca,'XTick',[125 250 500 1000 2000 4000 8000 16000 ]')    % Frequency points
 set(gca,'YTick',[-10 -5 0  ]') % dB scale
 axis([45 22500 -8 1])  %Scaling of axes
+% colo rmap(rgb(2:end, :));
 
 % plot the estimated frequency response for 1. segment
 subplot(3,1,3)
-semilogx(W*fs/(2*pi), mag2db(abs(H)));
-
+semilogx(W*fs/(2*pi), mag2db(abs(H)) + gain_offsets(1));
+% colormap(rgb(1, :));
 hold on
 
 for n = 1 : size(Acolor,2)
@@ -46,9 +50,8 @@ for n = 1 : size(Acolor,2)
     semilogx(W2*fs/(2*pi), mag2db(abs(H2)) + gain_offsets(n));
     offset= 10 + n * 2;
     hold on
-    
 end
-
+% colormap(rgb(2:end, :));
 
 ylabel('Magnitude');xlabel('Frequency');
 set(gca,'XTick',[125 250 500 1000 2000 4000 8000 16000]')    % Frequency points
---
+++ matlab/functions/rolloffIdx.m
@@ -0,0 +1,32 @@
+function [idxs, freqs, vals] = rolloffIdx(mags, rolloff, min_freq, Fs)
+    % idxs = rolloffIdx(mags, rolloff, min_freq, Fs)
+    %          INPUTS
+    %            mags : column-major matrix of NORMALIZED spectral magnitudes
+    %         rolloff : dB level to search for in rolloff countour
+    %        min_freq : search above this frequency (shrink the search space)
+    % freq_resolution : number of points comprising the full magnitude
+    %                   spectrum
+    %              Fs : sample rate
+    %    OUTPUTS
+    %       idxs : vector of indicies closest to the rolloff
+    %      freqs : frequencies at idxs
+    %       vals : values in mags matrix at indxs
+    
+    % narrow the search space: omit part of the spectrum below min_freq
+    nFreqs = length(mags);
+    minFrqIdx = floor(min_freq/(Fs/2) * nFreqs);
+    srchSpec = mags(minFrqIdx:end, :);
+    
+    % find closest values, by index
+    [d, ix] = min(abs(srchSpec - rolloff));
+    
+    % restore index from min_freq offset
+    idxs = ix + minFrqIdx-1;    
+    
+    %     cols = 1:size(mags, 2);
+    %     freqs = sub2ind(size(mags), idxs, cols);
+    %     freqs = w(ix)/pi * (Fs/2);
+    freqs = Fs/2 / nFreqs * (ix-1);
+    
+    vals = mags(idxs); % cutoff values in each segment
+end
